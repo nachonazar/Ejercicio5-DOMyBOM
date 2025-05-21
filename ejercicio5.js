17c8e8aca5d7726543b141a6b768d604ddef8cd9
Ignacio <ignacionazar9@gmail.com>
2025-05-21 02:06:40 -0300

    agregue la logica del boton iniciar del cronometro

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,5 +1,37 @@
-const iniciarCronómetro = () =>{
-    const btnIniciar = document.getElementById("btnIniciar")
-    
-}
-btnIniciar.addEventListener("click", iniciarCronómetro)
+let horas = 0;
+let minutos = 0;
+let segundos = 0;
+let timerId;
+const iniciarCronómetro = () => {
+  if (timerId) return;
+  timerId = setInterval(() => {
+    segundos++;
+    if (minutos === 60) {
+      minutos = 0;
+      horas++;
+    }
+    if (segundos === 60) {
+      segundos = 0;
+      minutos++;
+    }
+
+    let hora = horas
+    let minuto = minutos
+    let segundo = segundos
+
+    if (horas < 10) {
+      hora = "0" + horas;
+    }
+    if (minutos < 10) {
+      minuto = "0" + minutos;
+    }
+    if (segundos < 10) {
+      segundo = "0" + segundos;
+    }
+
+    const Cronómetro = document.querySelector(".fs-1");
+    Cronómetro.textContent = `${hora} : ${minuto} : ${segundo}`;
+  }, 1000);
+};
+const btnIniciar = document.getElementById("btnIniciar");
+btnIniciar.addEventListener("click", iniciarCronómetro);
